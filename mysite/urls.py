2d6c370802faf3ba6140a06c03563b244de8238f
k0825 <kazukkiyo@icloud.com>
2019-06-12 18:07:35 +0900

    tutorial4

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    #includeは他のURLconfへの参照をする
-    # http://localhost:8000/polls/へアクセスすると対応付けされたビューが見える
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
--- polls/models.py
+++ polls/models.py
@@ -2,25 +2,22 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-#各フィールドにどのようなデータ型を記憶させるかを書く。
-
-#Questionテーブル
+#Question
 class Question(models.Model):
-    question_text = models.CharField(max_length=200) #char型
-    pub_date = models.DateTimeField('date published') #date型
+    question_text = models.CharField(max_length=200) #char
+    pub_date = models.DateTimeField('date published') #date
 
-    #str(question)などと呼び出されたとき
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-#Choiceテーブル
+#Choice
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) #外部キー
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0) #Integer型
+    votes = models.IntegerField(default=0) #Integer
 
     def __str__(self):
         return self.choice_text
